4388a43c980dc56330b407346395003b712c358b
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-09-29 16:10:45 +1000

    feat: journal entries setter function now exists

--- src/contexts/EntriesContext.jsx
+++ src/contexts/EntriesContext.jsx
@@ -3,7 +3,8 @@ import { createContext, useContext, useState } from "react";
 
 // Create the context
 //      SomeContextVariable = createContext(defaultValue);
-const JournalEntriesContext = createContext([]);
+const JournalEntriesDataContext = createContext([]);
+const JournalEntriesSetterContext = createContext(null);
 
 // function SomeExample(){
 // const journalData = useContext(JournalEntriesContext);
@@ -12,12 +13,19 @@ const JournalEntriesContext = createContext([]);
 // Create custom hooks to access the context data
 export function useJournalEntriesData(){
     console.log("Passing data around!");
-    let currentJournalData = useContext(JournalEntriesContext);
+
+    let currentJournalData = useContext(JournalEntriesDataContext);
     if (currentJournalData.length == 0){
         console.log("No entries to show!");
     }
     return currentJournalData;
 }
+
+
+export function useJournalEntriesSetter(){
+    return useContext(JournalEntriesSetterContext);
+}
+
 
 // Create the context provider
 
@@ -25,8 +33,13 @@ export default function JournalEntriesProvider(props){
     let [journalEntries, setJournalEntries] = useState([]);
 
     return(
-        <JournalEntriesContext.Provider value={journalEntries}>
-            {props.children}
-        </JournalEntriesContext.Provider>
+
+        // <JournalEntriesContext.Provider value={[journalEntries, setJournalEntries]}>
+        <JournalEntriesDataContext.Provider value={journalEntries}>
+            <JournalEntriesSetterContext.Provider value={setJournalEntries}>
+                {props.children}
+            </JournalEntriesSetterContext.Provider>
+
+        </JournalEntriesDataContext.Provider>
     );
 }
